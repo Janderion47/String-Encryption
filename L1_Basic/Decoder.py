43e24642508a6a43eff73c78adf9b124f421f1b9
Jp Unknown <jp.unknown47@gmail.com>
2019-07-24 12:03:59 -0400

    Added L1

---
+++ L1_Basic/Decoder.py
@@ -0,0 +1,15 @@
+class Decoder:
+	"""This is to be used as an decoding tool for the encoding tool up above"""
+	
+	def __init__(self, decoding_factor):
+		self.decfac = int(decoding_factor)
+	
+	def dec(self):
+		will_be_decoded = int(input("What is to be decoded?: "))
+		
+		print("The following will be decoded: ", will_be_decoded)
+		
+		to_be_printed = chr(int(int(will_be_decoded) / int(self.decfac)))
+		print(to_be_printed)
+		
+		BasicEncoding.Decoder.dec(self)
---
+++ L1_Basic/Encoder.py
@@ -0,0 +1,17 @@
+class Encoder:
+	"""This is to be used as an encoding tool for scripts"""
+	
+	def __init__(self, encoding_factor):
+		self.encfac = int(encoding_factor)
+	
+	def enc(self):
+		to_be_worked_on = input("What do you want to be encoded?: ")
+		if to_be_worked_on == "That is it":
+			exit()
+		else:
+			print("The following will be encoded: ", str(to_be_worked_on))
+			
+			for i in to_be_worked_on:
+				to_be_added = int(ord(i) * int(self.encfac))
+				
+				print(to_be_added)
