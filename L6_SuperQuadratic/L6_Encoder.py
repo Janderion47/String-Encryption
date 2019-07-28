203d5438cb2c29b78d4c1f80819fca4ad4e4c436
Jp Unknown <jp.unknown47@gmail.com>
2019-07-28 14:29:57 -0400

    L6 created, not finished

---
+++ L6_SuperQuadratic/L6_Encoder.py
@@ -0,0 +1,37 @@
+class Encoder:
+	def __init__(self, a, b, c, steps = 10):
+		self.a = a[0] * a[1]
+		self.b = b[0] * b[1]
+		self.c = c[0] * c[1]
+		self.add = [a[2], b[2], c[2]]
+		self.steps = steps
+	
+	def enc(self):
+		a = self.a
+		b = self.b
+		c = self.c
+		s = self.steps
+		
+		def quad(x, a, b, c):
+			output = int((a * (x ** 2)) + (b * x) + c)
+			return output
+		
+		def do_over(steps, x, a, b, c):
+			memory = x
+			step = steps
+			if step >= 0:
+				memory == quad(memory, a, b, c)
+				step -= 1
+			elif step == 0:
+				return memory
+		
+		to_be_encoded = input("What do you wish to be encoded master?: ")
+		
+		for i in to_be_encoded:
+			character = int(ord(i))
+			
+			a += self.add[0]
+			b += self.add[1]
+			c += self.add[2]
+			
+			print(part_b)
